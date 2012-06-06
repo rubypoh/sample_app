55ff37004bfb4106100b7376975612c60ffe45a6
Graig Alpert <gmalpert@gmail.com>
2012-06-06 14:44:56 -0400

    Install Postgresql

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
   
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have the h1 'Sample App'" do
@@ -42,6 +44,20 @@ describe "Static pages" do
     	visit '/static_pages/about'
 	page.should have_selector('title', 
 				      :text => " | About Us")
+	  end
+  end
+
+      describe "Contact page" do
+
+	  it "should have the content 'Contact'" do
+		  visit '/static_pages/contact'
+	    page.should have_selector('h1', :text => 'Contact')
+    end
+
+	it "should have the title 'Contact'" do
+    	visit '/static_pages/contact'
+	page.should have_selector('title', 
+				      :text => " | Contact")
 	  end
   end
 end
